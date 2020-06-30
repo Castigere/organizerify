3fed5a16fc6594e8b6f5dfe2b1383043f28a426a
Even Haasted <evenhaasted@gmail.com>
2020-06-30 18:58:56 +0200

    deploy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const API_VERSION = 'api';
 const HOSTNAME = window.location.hostname;
 const PROTOCOL = window.location.protocol;
-const WEBSOCKET_PROTOCOL = HOSTNAME === 'localhost' ? 'ws:' : 'wss';
+const WEBSOCKET_PROTOCOL = HOSTNAME === 'localhost' ? 'ws:' : 'wss:';
 const PORT = HOSTNAME === 'localhost' ? '8080' : '443';
 const FACEBOOK_AUTH_ENDPOINT = `${PROTOCOL}//${HOSTNAME}:${PORT}/${API_VERSION}/auth/facebook`;
 const GOOGLE_AUTH_ENDPOINT = `${PROTOCOL}//${HOSTNAME}:${PORT}/${API_VERSION}/auth/google`;
